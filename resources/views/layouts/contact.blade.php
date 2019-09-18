a081869f3c5b905405635a80c60d17983ba27707
developerazad <developerazad.hstu@gmail.com>
2019-09-19 00:21:42 +0600

    contact details

--- resources/views/layouts/contact.blade.php
+++ resources/views/layouts/contact.blade.php
@@ -4,11 +4,11 @@
             <div class="contact-cont">
                 <h3>CONTACT Us</h3>
                 <div class="heading-border-light"></div>
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
+                <p>It's a pleasure for me to get contacted by you. If you have any query just feel free to inform me at your convenience.</p>
             </div>
             <div class="row con-form">
                 <div class="col-md-12">
-                    <input type="text" name="full-name" placeholder="Full Name" class="form-control">
+                    <input type="text" name="full-name" placeholder="Name" class="form-control">
                 </div>
                 <div class="col-md-12">
                     <input type="text" name="email" placeholder="Email" class="form-control">
@@ -16,7 +16,7 @@
                 <div class="col-md-12">
                     <input type="text" name="subject" placeholder="Subject" class="form-control">
                 </div>
-                <div class="col-md-12"><textarea name="" id=""></textarea></div>
+                <div class="col-md-12"><textarea name="" id="" ></textarea></div>
                 <div class="col-md-12 sub-but"><button class="btn btn-general btn-white" role="button">Send</button></div>
             </div>
         </div>
@@ -24,23 +24,22 @@
             <div class="contact-cont2">
                 <div class="contact-add contact-box-desc">
                     <h3><i class="fa fa-map-marker cl-atlantis fa-2x"></i> Address</h3>
-                    <p>25, Dist town Street, Logn <br>
-                        California, US <br></p>
+                    <p>Uttara Model Town, Sector#7 <br>
+                        Dhaka, 1230 <br></p>
                 </div>
                 <div class="contact-phone contact-side-desc contact-box-desc">
                     <h3><i class="fa fa-phone cl-atlantis fa-2x"></i> Phone</h3>
-                    <p>800 123 3456 <br>900 123 3457</p>
+                    <p>+880176 4761919 <br>+880152 1470368</p>
                 </div>
                 <div class="contact-mail contact-side-desc contact-box-desc">
                     <h3><i class="fa fa-envelope-o cl-atlantis fa-2x"></i> Email</h3>
                     <address class="address-details-f">
-                        Fax: 800 123 3456 <br>
-                        Email: <a href="mailto:info@themsbit.com" class="">info@themsbit.com</a>
+                        Email: <a href="mailto:developerazad.hstu@gmail.com" class="">developerazad.hstu@gmail.com</a>
                     </address>
                     <ul class="list-inline social-icon-f top-data">
-                        <li><a href="#" target="_empty"><i class="fa top-social fa-facebook" style="color: #4267b2; border-color:#4267b2;"></i></a></li>
-                        <li><a href="#" target="_empty"><i class="fa top-social fa-twitter" style="color: #4AB3F4; border-color:#4AB3F4;"></i></a></li>
-                        <li><a href="#" target="_empty"><i class="fa top-social fa-google-plus" style="color: #e24343; border-color:#e24343;"></i></a></li>
+                        <li><a href="https://www.facebook.com/developerazad" target="_empty"><i class="fa top-social fa-facebook" style="color: #4267b2; border-color:#4267b2;"></i></a></li>
+                        <li><a href="https://linkedin.com/in/devloperazad" target="_empty"><i class="fa top-social fa-linkedin" style="color: #4AB3F4; border-color:#4AB3F4;"></i></a></li>
+                        <li><a href="https://github.com/developerazad" target="_empty"><i class="fa top-social fa-github" style="color: #e24343; border-color:#e24343;"></i></a></li>
                     </ul>
                 </div>
             </div>
