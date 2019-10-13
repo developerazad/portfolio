ce1904551b131d729e4d891f4c081fc12636cb5d
developerazad <developerazad.hstu@gmail.com>
2019-10-13 23:15:16 +0600

    experience

--- resources/views/layouts/experience.blade.php
+++ resources/views/layouts/experience.blade.php
@@ -1,8 +1,9 @@
 <style>
     section.resume-section {
         border-bottom: 1px solid #dee2e6;
-        padding-top: 5rem !important;
-        padding-bottom: 0!important;
+        padding-top: 1rem !important;
+        padding-bottom: 3rem!important;
+        min-height: 300px!important;
     }
 </style>
 
@@ -13,22 +14,36 @@
             <div class="mb-5 heading-border"></div>
         </div>
         <div class="resume-item col-md-6 col-sm-12 " >
-            <div class="card mx-0 p-4 mb-5" style="border-color: #17a2b8; box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.21);">
+            <div class="card mx-0 p-4 mb-1" style="border-color: #17a2b8; box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.21);">
                 <div class=" resume-content mr-auto">
                     <h4 class="mb-3"><i class="fa fa-graduation-cap mr-3 text-info"></i> ATI Limited <span class="text-info" style="font-size: 15px;">Junior PHP Programmer</span></h4>
                     <p>One of the leading software company in Bangladesh. Working as a junior php programmer. Creating interactive web applications mostly in Laravel and CodeIgniter. Working with lots of amazing professional, learning and developing myself very smoothly.</p>
                 </div>
+                <ul class="list-groups">
+                    <p class="text-left">Job Responsibilities:</p>
+                    <li class="list-group-items">Design, develop, test and deploy web applications</li>
+                    <li class="list-group-items">Develop appropriate code structure to solve specific task</li>
+                    <li class="list-group-items">Fix bugs, troubleshoot and resolve problems</li>
+                    <li class="list-group-items">Coordinate with others designers and developers to develop web applications</li>
+                </ul>
                 <div class="resume-date text-md-right">
                     <span class="text-primary"> August 2018 - Present</span>
                 </div>
             </div>
         </div>
         <div class="resume-item col-md-6 col-sm-12">
-            <div class="card mx-0 p-4 mb-5" style="border-color: #17a2b8; box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.21);">
+            <div class="card mx-0 p-4 mb-1" style="border-color: #17a2b8; box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.21);">
                 <div class="resume-content mr-auto">
                     <h4 class="mb-3"><i class="fa fa-graduation-cap mr-3 text-info"></i> Northern Tech BD  <span class="text-info" style="font-size: 15px;">Junior Web Developer</span></h4>
                     <p>This is the first IT firm where I started my career to implement my knowledge achieved in my university life. Here I worked as a junior web developer. Have designed and developed some projects. I am grateful to this company.</p>
                 </div>
+                <ul class="list-groups">
+                    <p class="text-left">Job Responsibilities:</p>
+                    <li class="list-group-items">Design, develop, test and deploy web applications</li>
+                    <li class="list-group-items">Develop appropriate code structure to solve specific task</li>
+                    <li class="list-group-items">Fix bugs, troubleshoot and resolve problems</li>
+                    <li class="list-group-items">Coordinate with others designers and developers to develop web applications</li>
+                </ul>
                 <div class="resume-date text-md-right">
                     <span class="text-primary"> January 2018 - Jun 2018</span>
                 </div>
